1ee9935044bde6b71d8cb0d557431fd443a6ad43
Sriram26231 <74018096+Sriram26231@users.noreply.github.com>
2021-02-19 15:45:20 +0000

    Add files via upload

--- Project 27/MatterJSBoilerPlate-master/sketch.js
+++ Project 27/MatterJSBoilerPlate-master/sketch.js
@@ -3,7 +3,9 @@ const World= Matter.World;
 const Bodies = Matter.Bodies;
 
 var engine, world;
-var ground, ball;
+var ground, ball1, ball2, ball3, ball4, ball5;
+var rope1, rope2, rope3, rope4, rope5;
+var roof;
 
 function setup(){
     var canvas = createCanvas(400,400);
@@ -21,28 +23,30 @@ function setup(){
         restitution: 1.0
     }
 
-    ball1 = Bodies.circle(200,100,20, ball_options);
+    ball1 = Bodies.circle(100,100,20, ball_options);
     World.add(world,ball1);
-	ball2 = Bodies.circle(200,100,20, ball_options);
+	ball2 = Bodies.circle(150,100,20, ball_options);
     World.add(world,ball2);
 	ball3 = Bodies.circle(200,100,20, ball_options);
     World.add(world,ball3);
-	ball4 = Bodies.circle(200,100,20, ball_options);
+	ball4 = Bodies.circle(250,100,20, ball_options);
     World.add(world,ball4);
-	ball5 = Bodies.circle(200,100,20, ball_options);
+	ball5 = Bodies.circle(300,100,20, ball_options);
     World.add(world,ball5)
+    roof = Bodies.rectangle(200,20,300,20)
+
+    rope1 = new Rope(ball1.body,roof.body,-ball1Diameter*2, 0)
 
 
  
 
-    console.log(ground);
 }
 
 function draw(){
     background(0);
     Engine.update(engine);
     rectMode(CENTER);
-    rect(200,20,300,20);
+    rect(roof.position.x,roof.position.y,300,20);
 	ellipseMode(RADIUS);
     ellipse(ball1.position.x, ball1.position.y, 20, 20);
 	ellipseMode(RADIUS);
@@ -53,5 +57,7 @@ function draw(){
     ellipse(ball4.position.x, ball4.position.y, 20, 20);
 	ellipseMode(RADIUS);
     ellipse(ball5.position.x, ball5.position.y, 20, 20);
+
+    
 
 }
---
+++ Project 27/Rope.js
@@ -0,0 +1,16 @@
+class Rope{
+    constructor(body1, body2, offsetX,offsetY){
+
+        this.offsetX = this.offsetX
+        this.offsetY = this.offsetY
+        var options = {
+            bodyA: body1,
+            bodyB: body2,
+            pointB:{x:this.offsetX, y:this.offsetY}
+        }
+        
+        this.rope = Constraint.create(options);
+        World.add(world, this.rope);
+    }
+
+}
